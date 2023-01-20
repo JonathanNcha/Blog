c2adbf2939e17c284f5ec6025a3fc256433f7dd9
JonathanNcha <nchajonathan@gmail.com>
2023-01-20 20:26:02 +0100

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+// const marked = require('marked');
+const { marked } = require('marked')
+const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const { hash } = require('bcrypt');
+const dompurify = createDomPurify(new JSDOM().window);
+
+
+const userSchema = new mongoose.Schema({
+    id: {
+        type: String
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+module.exports = mongoose.model('User',userSchema);
